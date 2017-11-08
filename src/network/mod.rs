90ce07fd89a95020a62f39f86b8d286c222e796b
sirkibsirkib <christopher.esterhuyse@gmail.com>
2017-11-08 09:01:36 +0100

    pruned dead code

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -123,48 +123,6 @@ pub fn bounded_printable(b : BoundedString) -> String {
 pub type ClientID = u16;
 pub const SINGLE_PLAYER_CID : ClientID = 0;
 
-// //PRIMITIVE
-// #[derive(Serialize, Deserialize, Copy, Clone, Debug)]
-// pub enum MsgToServer {
-//     RequestControlOf(EntityID),
-//     RelinquishControlof(EntityID),
-//     CreateEntity(EntityID,Point),
-//     ControlMoveTo(LocationID,EntityID,Point),
-//     //username, password_hash
-//     ClientLogin(BoundedString,BoundedString),
-//     RequestEntityData(EntityID),
-//     RequestControlling,
-//     RequestLocationData(LocationID),
-// }
-//
-// //PRIMITIVE
-// #[derive(Serialize, Deserialize, Copy, Clone, Debug)]
-// pub enum MsgToClient {
-//     GiveEntityData(EntityID,LocationID,Point),
-//     GiveControlling(Option<(EntityID,LocationID)>),
-//     CreateEntity(EntityID,Point),
-//     YouNowControl(EntityID),
-//     YouNoLongerControl(EntityID),
-//     EntityMoveTo(EntityID,Point),
-//     LoginSuccessful(ClientID),
-//     LoginFailure(UserBaseError),
-// }
-//
-//
-// //WRAPS MsgToServer
-// #[derive(Serialize, Deserialize, Copy, Clone, Debug)]
-// pub struct MsgFromClient {
-//     pub msg : MsgToServer,
-//     pub cid : ClientID,
-// }
-//
-// //WRAPS MsgToClient
-// #[derive(Serialize, Deserialize, Copy, Clone, Debug)]
-// pub enum MsgToClientSet {
-//     Only(MsgToClient, ClientID),
-//     All(MsgToClient),
-// }
-
 pub struct ProtectedQueue<T> {
     queue : Mutex<Vec<T>>,
     cond : Condvar,
@@ -188,10 +146,10 @@ impl<T> ProtectedQueue<T> {
         self.cond.notify_all();
     }
 
-    // pub fn lock_len(&mut self) -> usize {
-    //     let locked_queue = self.queue.lock().unwrap();
-    //     locked_queue.len()
-    // }
+    pub fn lock_len(&mut self) -> usize {
+        let locked_queue = self.queue.lock().unwrap();
+        locked_queue.len()
+    }
 
     pub fn lock_push_notify(&self, t : T) {
         let mut locked_queue = self.queue.lock().unwrap();
